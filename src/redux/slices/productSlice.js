c2b98f681eb3216fc36c84717e92d1fed4f599f0
Shrawan <shrwnkumar08@gmail.com>
2025-10-06 22:38:05 +0530

    product reducer function implemented

---
+++ src/redux/slices/productSlice.js
@@ -0,0 +1,41 @@
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+import { api } from "../../config/config";
+import axios from "axios";
+
+const initialState = {
+    products : [],
+    error : null,
+    loading : false
+}
+
+export const getProducts = createAsyncThunk("getProducts",async({rejectWithValue})=>{
+    try {
+        const response = await axios.get(`${api}/products`);
+        return response.data;
+    } catch (error) {
+        return rejectWithValue(error.response?.data || error.message)
+    }
+})
+export const productSlice = createSlice({
+    name : "products",
+    initialState,
+
+    reducers :{},
+
+    extraReducers:(builder)=>{
+        builder.addCase(getProducts.pending, (state)=>{
+            state.error = null;
+            state.loading = true;
+        })
+        .addCase(getProducts.fulfilled, (state,action)=>{
+            state.loading = false;
+            state.products = action.payload;
+        })
+        .addCase(getProducts.rejected, (state,action)=>{
+            state.loading = false;
+            state.products = action.payload;
+        })
+    }
+})
+
+export default productSlice.reducer;
