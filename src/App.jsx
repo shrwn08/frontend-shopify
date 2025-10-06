5f8144296c181e601a66b4c0931bd6476989fc58
Shrawan <shrwnkumar08@gmail.com>
2025-10-07 01:07:00 +0530

    route fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { Suspense } from "react";
 import "./App.css";
 import router from "./routes/Routes";
 import { RouterProvider } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 
 function App() {
   return (
-   <Suspense fallback={<div>Loading...</div>}>
-      <RouterProvider router={router} />
-   </Suspense>
-  )
-};
+    <Provider store={store}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <RouterProvider router={router} />
+      </Suspense>
+    </Provider>
+  );
+}
 
 export default App;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,33 +1,36 @@
-import React from 'react'
-import { lazy } from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import React from "react";
+import { lazy } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../Layout/Layout.jsx";
 
-const Login = lazy(()=>import("../pages/Login.jsx"));
-const Register = lazy(()=>import("../pages/Register.jsx"));
-const Home = lazy(()=> import("../pages/Home.jsx"));
-const ProductDetails = lazy(()=>("../pages/ProductDetails.jsx"));
-
+const Login = lazy(() => import("../pages/Login.jsx"));
+const Register = lazy(() => import("../pages/Register.jsx"));
+const ProductList = lazy(() => import("../pages/ProductList.jsx"));
+const ProductDetails = lazy(() => import("../pages/ProductDetails.jsx"));
 
 const router = createBrowserRouter([
-    {
-        path : "/",
-        element : <Home />
-    },
-    {
-        path : "/register",
-        element : <Register />
-    },
-    {
-        path : "/login",
-        element : <Login />
-    },
-    {
-        path : "/product/:id",
-        element : <ProductDetails />
-    }
-])
-
-
-
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <ProductList />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/product/:id",
+        element: <ProductDetails />,
+      },
+    ],
+  },
+]);
 
 export default router;
