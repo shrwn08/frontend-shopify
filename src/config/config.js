86272e7c473cf1365feb36c1c69acdfd17496c1b
Shrawan <shrwnkumar08@gmail.com>
2025-10-13 00:50:42 +0530

    adding .env in the code but not on github

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,5 @@
-export const api = 'http://localhost:8080/api';
+
+export const api =
+  import.meta.env.MODE === 'development'
+    ? 'http://localhost:8080/api'
+    : 'https://backend-shopify-oqt1.onrender.com/api'
